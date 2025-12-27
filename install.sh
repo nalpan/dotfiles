cc3f540ea8c088b07804a2aaad413d04ed74fdda
nalpan <nakata.kzhr@gmail.com>
2025-12-27 19:42:49 +0900

    install check and backup

--- install.sh
+++ install.sh
@@ -2,6 +2,13 @@ DIR=~/dotfiles
 
 echo $DIR
 
+# check if files are already linked
+[[ -L ~/.zshrc ]] && [[ -L ~/.gitconfig ]] && echo "All files are already linked!" && exit 0
+
+# backup existing files
+[[ ! -f $DIR/backup/.zshrc ]] && mv ~/.zshrc $DIR/backup/.zshrc
+[[ ! -f $DIR/backup/.gitconfig ]] && mv ~/.gitconfig $DIR/backup/.gitconfig
+
 # create link
 [[ ! -f ~/.zshrc ]] && ln -s $DIR/.zshrc ~/.zshrc
 [[ ! -f ~/.gitconfig ]] && ln -s $DIR/.gitconfig ~/.gitconfig
