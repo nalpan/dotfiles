d9d79c806a0e4e63c45be743f1c0f817d167e19c
nalpan <nakata.kzhr@gmail.com>
2026-01-25 21:31:41 +0900

    add CLAUDE.md

--- install.sh
+++ install.sh
@@ -16,13 +16,15 @@ echo $DIR
 [[ ! -f $DIR/backup/AGENTS.md ]] && mv ~/AGENTS.md $DIR/backup/AGENTS.md
 [[ -f ~/.config/git/ignore ]] && [[ ! -f $DIR/backup/.config/git/ignore ]] && mv ~/.config/git/ignore $DIR/backup/.config/git/ignore
 [[ -f ~/.copilot/copilot-instructions.md ]] && [[ ! -f $DIR/backup/.copilot/copilot-instructions.md ]] && mv ~/.copilot/copilot-instructions.md $DIR/backup/.copilot/copilot-instructions.md
+[[ -f ~/.claude/CLAUDE.md ]] && [[ ! -f $DIR/backup/.claude/CLAUDE.md ]] && mv ~/.claude/CLAUDE.md $DIR/backup/.claude/CLAUDE.md
 
 # create directory and link
 [[ ! -f ~/.zshrc ]] && ln -s $DIR/.zshrc ~/.zshrc
 [[ ! -f ~/.gitconfig ]] && ln -s $DIR/.gitconfig ~/.gitconfig
 [[ ! -f ~/AGENTS.md ]] && ln -s $DIR/AGENTS.md ~/AGENTS.md
-mkdir -p ~/.config/git && [[ ! -f ~/.config/git/ignore ]] && ln -s $DIR/.config/git/ignore ~/.config/git/ignore
-mkdir -p ~/.copilot && [[ ! -f ~/.copilot/copilot-instructions.md ]] && ln -s $DIR/.copilot/copilot-instructions.md ~/.copilot/copilot-instructions.md
+mkdir -p ~/.config/git && ln -s $DIR/.config/git/ignore ~/.config/git/ignore
+mkdir -p ~/.copilot && ln -s $DIR/.copilot/copilot-instructions.md ~/.copilot/copilot-instructions.md
+mkdir -p ~/.claude && ln -s $DIR/.claude/CLAUDE.md ~/.claude/CLAUDE.md
 
 echo "All files are linked!"
 exit 0
