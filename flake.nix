16ec4766d788a91b3e29cd09e6b324d6dc01ad86
nalpan <nakata.kzhr@gmail.com>
2026-08-11 18:30:36 +0900

    backup config

--- flake.nix
+++ flake.nix
@@ -32,6 +32,7 @@
             {
               home-manager.useGlobalPkgs = true;
               home-manager.useUserPackages = true;
+              home-manager.backupFileExtension = "backup";
               home-manager.extraSpecialArgs = { inherit username nix-claude-code; };
               home-manager.users.${username} = import ./nix/home.nix;
             }
